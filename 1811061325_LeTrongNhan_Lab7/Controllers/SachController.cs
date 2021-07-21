89b3479dc760a62dc6cf5ba43f1d3b566e8503ed
trongnhanle <letrongnhan1410200@gmail.com>
2021-07-21 13:10:39 +0700

    Add project files.

---
+++ 1811061325_LeTrongNhan_Lab7/Controllers/SachController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _1811061325_LeTrongNhan_Lab7.Controllers
+{
+    public class SachController : ApiController
+    {
+
+    }
+}
---
+++ 1811061325_LeTrongNhan_Lab7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811061325_LeTrongNhan_Lab7.WebApiApplication" Language="C#" %>
